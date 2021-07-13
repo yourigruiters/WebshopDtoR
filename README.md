84ab9973f978ac03e59ac18fb55e0eba096c9d36
Youri Gruiters <youri.gruiters@ui.com>
2021-07-13 22:16:42 +0200

    Cleanup July 2021

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-![Logo YG](http://github.yourigruiters.com/images/logo.png)
-
 # React webshop
+
 React webshop made from previous HTML design
 
 ## Used Modules
 
-* Create-react-app
-* React-router-dom
-* Font-awesome
-* Bootstrap
-
-## Images 
-
-![Image1](http://github.yourigruiters.com/images/webshop1.png)
-![Image2](http://github.yourigruiters.com/images/webshop2.png)
+- Create-react-app
+- React-router-dom
+- Bootstrap
+- Class components
--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
-import Home from './components/Home';
-import Shop from './components/Shop';
-import About from './components/About';
-import Contact from './components/Contact';
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-import CompleteProduct from './components/products/CompleteProduct';
+import CompleteProduct from "./components/products/CompleteProduct";
 
-import Login from './components/auth/Login';
-import Cart from './components/cart/Cart';
+import Login from "./components/auth/Login";
+import Cart from "./components/cart/Cart";
 
-import Error404 from './components/error/Error404';
+import Error404 from "./components/error/Error404";
 
 class App extends Component {
   state = {
     products: [
-      {id: 1, title: "Product 1", content: "Content...", price: 39.99},
-      {id: 2, title: "Product 2", content: "Content...", price: 29.99},
-      {id: 3, title: "Product 3", content: "Content...", price: 19.99},
-      {id: 4, title: "Product 4", content: "Content...", price: 25.99},
+      { id: 1, title: "Product 1", content: "Content...", price: 39.99 },
+      { id: 2, title: "Product 2", content: "Content...", price: 29.99 },
+      { id: 3, title: "Product 3", content: "Content...", price: 19.99 },
+      { id: 4, title: "Product 4", content: "Content...", price: 25.99 }
     ],
     cartItems: 0
-  }
+  };
 
   componentDidMount() {
     let cartAmount;
@@ -38,7 +38,7 @@ class App extends Component {
 
     this.setState({
       cartItems: cartAmount
-    })    
+    });
   }
 
   changeCart = () => {
@@ -51,8 +51,8 @@ class App extends Component {
 
     this.setState({
       cartItems: cartAmount
-    })
-  }
+    });
+  };
 
   render() {
     return (
@@ -60,19 +60,59 @@ class App extends Component {
         <div className="webshop">
           <Header cartItems={this.state.cartItems} />
           <Switch>
-            <Route path="/" exact render={(props) => <Home {...props} products={this.state.products} changeCart={this.changeCart} /> } />
-            <Route path="/shop" exact render={(props) => <Shop {...props} products={this.state.products} changeCart={this.changeCart} /> } />
-            <Route path="/shop/:id" exact render={(props) => <CompleteProduct {...props} products={this.state.products} changeCart={this.changeCart} /> } />
-            <Route path="/about" exact component={About} /> 
-            <Route path="/contact" exact component={Contact} /> 
-            <Route path="/login" exact component={Login} /> 
-            <Route path="/cart" exact render={(props) => <Cart {...props} products={this.state.products} changeCart={this.changeCart} /> } />
+            <Route
+              path="/"
+              exact
+              render={(props) => (
+                <Home
+                  {...props}
+                  products={this.state.products}
+                  changeCart={this.changeCart}
+                />
+              )}
+            />
+            <Route
+              path="/shop"
+              exact
+              render={(props) => (
+                <Shop
+                  {...props}
+                  products={this.state.products}
+                  changeCart={this.changeCart}
+                />
+              )}
+            />
+            <Route
+              path="/shop/:id"
+              exact
+              render={(props) => (
+                <CompleteProduct
+                  {...props}
+                  products={this.state.products}
+                  changeCart={this.changeCart}
+                />
+              )}
+            />
+            <Route path="/about" exact component={About} />
+            <Route path="/contact" exact component={Contact} />
+            <Route path="/login" exact component={Login} />
+            <Route
+              path="/cart"
+              exact
+              render={(props) => (
+                <Cart
+                  {...props}
+                  products={this.state.products}
+                  changeCart={this.changeCart}
+                />
+              )}
+            />
             <Route path="/" component={Error404} />
           </Switch>
           <Footer />
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
